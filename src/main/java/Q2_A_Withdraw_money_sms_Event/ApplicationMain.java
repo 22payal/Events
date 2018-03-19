21e45b8ac361d21f72ccde51e5bd0c1a5997b17d
22payal <payalnigam1996@gmail.com>
2018-03-19 11:28:45 +0530

    Update ApplicationMain.java

--- src/main/java/Q2_A_Withdraw_money_sms_Event/ApplicationMain.java
+++ src/main/java/Q2_A_Withdraw_money_sms_Event/ApplicationMain.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class ApplicationMain {
     public static void main(String[] args) {
         ApplicationContext applicationContext=new AnnotationConfigApplicationContext(ApplicationConfig.class);
-       /* JdbcTemplate jdbcTemplate=applicationContext.getBean(JdbcTemplate.class);
-        jdbcTemplate.update("insert into user (name,pin,amount,mobile) VALUES (?,?,?,?)",new Object[]{"ankit",1234,"30000","9717516550"});*/
 
        Myuser user=applicationContext.getBean(Myuser.class);
        user.setUname("payal");
